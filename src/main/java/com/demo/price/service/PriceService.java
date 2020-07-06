bebc95516df6244f68936df39859b04e33c146c3
rAKESH <way2rakeshr@gmail.com>
2020-07-06 11:11:23 +0530

    qwerty

--- src/main/java/com/demo/price/service/PriceService.java
+++
@@ -1,8 +0,0 @@
-package com.demo.price.service;
-
-import com.demo.price.entity.Price;
-
-
-public interface PriceService  {
-	Price findprice(int pid);
-}
--- src/test/java/com/demo/price/PriceApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.demo.price;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PriceApplicationTests {
-
-	
-}
